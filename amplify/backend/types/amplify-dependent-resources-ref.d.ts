d6584cb16defb0f2e38365977e1eee8fd0d292ec
akira onishi <akira.frlnc@gmail.com>
2024-07-05 16:56:37 +0200

    implemented Amplify Authenticator

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awswithreact3bb2c444": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+import awsExports from "./aws-exports";
+
+Amplify.configure(awsExports);
+
+function App({signOut, user}) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Hello React app with AWS</h2>
+				{user ? (
+					<>
+					<h3>You are authenticated as {user.username}</h3>
+					<button onClick={signOut}>Sign Out</button>
+					</>
+				) : (
+					<h3>Please sign in</h3>
+				)}
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
